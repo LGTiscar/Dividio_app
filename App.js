240022759ccb054d90f95559efce56cd8658b701
Luis Garcia <luisgt@ext.inditex.com>
2023-03-07 09:22:37 +0100

    Declared Bill object

--- App.js
+++ App.js
@@ -3,107 +3,16 @@ import { StyleSheet, Text, View, Button, Image, TouchableOpacity  } from 'react-
 import { Camera } from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
 
-export default function Add({ navigation }) {
-  const [cameraPermission, setCameraPermission] = useState(null);
-  const [galleryPermission, setGalleryPermission] = useState(null);
-
-  const [camera, setCamera] = useState(null);
-  const [imageUri, setImageUri] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
-
-  const permisionFunction = async () => {
-    // here is how you can get the camera permission
-    const cameraPermission = await Camera.requestCameraPermissionsAsync();
-
-    setCameraPermission(cameraPermission.status === 'granted');
-
-    const imagePermission = await ImagePicker.getMediaLibraryPermissionsAsync();
-    console.log(imagePermission.status);
-
-    setGalleryPermission(imagePermission.status === 'granted');
-
-    if (
-      imagePermission.status !== 'granted' &&
-      cameraPermission.status !== 'granted'
-    ) {
-      alert('Permission for media access needed.');
-    }
-  };
-
-  useEffect(() => {
-    permisionFunction();
-  }, []);
-
-  const takePicture = async () => {
-    if (camera) {
-      const data = await camera.takePictureAsync(null);
-      console.log(data.uri);
-      setImageUri(data.uri);
-    }
-  };
-
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [1, 1],
-      quality: 1,
-    });
-
-    console.log(result);
-    if (!result.canceled) {
-      setImageUri(result.uri);
-    }
-  };
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.cameraContainer}>
-        <Camera
-          ref={(ref) => setCamera(ref)}
-          style={styles.fixedRatio}
-          type={type}
-          ratio={'1:1'}
-        />
-      </View>
-      <View style={{alignItems: 'center'}}>
-        <TouchableOpacity style={styles.button} onPress={takePicture}>
-          <Text style={{color: 'white'}}>Take Picture</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={pickImage}>
-          <Text style={{color: 'white'}}>Gallery</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+const Bill = {
+  Bill: {
+      Item1 : {
+          Price: Price1, clients : [Luis, Andrea]
+      },
+      Item2 : {
+          Price: Price2, clients: [Luis, Andrea, Dasua]
+      },
+      Item3 : {
+          Price: Price3, clients: [Luis, Dani, Dasua]
+      },
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#EDF1D6',
-  },
-  cameraContainer: {
-    flex: 1,
-    flexDirection: 'row',
-  },
-  fixedRatio: {
-    flex: 1,
-    aspectRatio: 1,
-  },
-  buttonContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  button: {
-    backgroundColor: '#9DC08B',
-    padding: 10,
-    borderRadius: 5,
-    margin: 10,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
-  },
-});
